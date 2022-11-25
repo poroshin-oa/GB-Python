7af5fb493e92f9e2c63cf60c1e7c29100d8a614a
oleg <poroshin.oa72@gmail.com>
2022-11-25 06:15:08 +0300

    **РЕШЕНО** Задача 1

--- Seminar_1-DZ/task_1.py
+++ Seminar_1-DZ/task_1.py
@@ -5,11 +5,19 @@
 # - 7 -> да
 # - 1 -> нет
 
-input_day_number = int(input("Input day number> "))
+input_day_number = input("Input day number> ")
 
-if input_day_number == 6 or input_day_number == 7:
-    print("This is weekend.")
-elif input_day_number in range(1, 6):
-    print("This is weekday.")
+if not input_day_number.isdigit():
+    print("This is not a number!")
 else:
-    print("Error!")
+    input_day_number = int(input_day_number)
+    
+    if input_day_number == 6 or input_day_number == 7:
+        print("This is weekend.")
+    elif input_day_number in range(1, 6):
+        print("This is weekday.")
+    else:
+        print("Number is out of range 1 - 7!")
+
+
+    
