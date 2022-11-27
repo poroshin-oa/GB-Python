8c463477a35bc5e284da4ed5cf5469ff11254f6e
oleg <poroshin.oa72@gmail.com>
2022-11-27 21:24:36 +0300

    **TASK_4** COMPLETE

--- Seminar_1-DZ/task_4.py
+++ Seminar_1-DZ/task_4.py
@@ -1,1 +1,15 @@
 # Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y).
+
+quarterNumber = int(input("Insert quarter number> "))
+
+if quarterNumber > 4 or quarterNumber <= 0:
+    print("Error! Quarter number must be in the range 1-4")
+else:
+    if quarterNumber == 1:
+        print("Range of X: 0 -> +infinity \nRange of Y: 0 -> +infinity")
+    elif quarterNumber == 2:
+        print("Range of X: 0 -> -infinity \nRange of Y: 0 -> +infinity")
+    elif quarterNumber == 3:
+        print("Range of X: 0 -> -infinity \nRange of Y: 0 -> -infinity")
+    elif quarterNumber == 4:
+        print("Range of X: 0 -> +infinity \nRange of Y: 0 -> -infinity")
