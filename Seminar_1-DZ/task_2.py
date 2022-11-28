6e1c7c72cb5dcd3bb8c4f9e76b3f54e0afe3059d
oleg <poroshin.oa72@gmail.com>
2022-11-28 06:05:30 +0300

    **TASK_2** Complete

--- Seminar_1-DZ/task_2.py
+++ Seminar_1-DZ/task_2.py
@@ -3,6 +3,21 @@
 # ⋁ - or
 # ¬ - not
 
-def generatePredicateList():
-    for i in range(3):
-        pass
+predicate_array_names = ["X", "Y", "Z"]
+predicate_range = [0, 1]
+predicate_array = []
+
+for i in range(3):
+    predicate_input = int(input("Input {}> ".format(predicate_array_names[i])))
+    
+    if predicate_input not in predicate_range:
+        print("Error!")
+        break
+    else:
+        predicate_array.append(predicate_input)
+
+left = not (predicate_array[0] or predicate_array[1] or predicate_array[2])
+right = not predicate_array[0] and not predicate_array[1] and not predicate_array[2]
+result = left == right
+
+print(result)
