382e90077dfacd2eb23cdd5718449c6513eb72d8
oleg <poroshin.oa72@gmail.com>
2022-11-29 06:04:04 +0300

    **TASK_1** Complete

--- Seminar_2-DZ/task_1.py
+++ Seminar_2-DZ/task_1.py
@@ -1,3 +1,20 @@
 # Напишите программу, которая принимает на вход вещественное число и показывает сумму его цифр.
 # Пример:
 # 0,56 -> 11
+
+float_input = input("Input float> ")
+
+if float_input.find(",") > 0:
+    separator = ","
+elif float_input.find(".") > 0:
+    separator = "."
+
+float_no_separator = float_input.replace(separator, "")
+
+float_number_sum = 0
+float_number_length = len(float_no_separator)
+
+for i in range(float_number_length):
+    float_number_sum += int(float_no_separator[i])
+
+print(float_number_sum)
