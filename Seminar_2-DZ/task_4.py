26b10933086291ccadcbdd0623411f83b91f4200
oleg <poroshin.oa72@gmail.com>
2022-11-30 06:30:34 +0300

    **TASK_4** Complete

--- Seminar_2-DZ/task_4.py
+++ Seminar_2-DZ/task_4.py
@@ -5,3 +5,27 @@
 # --> 0 2 3
 # -3 * -1 * 0 = 0
 # Вывод: 0
+
+input_number = int(input("Insert N> "))
+main_list = [i for i in range(-input_number, input_number)]
+main_list_length = len(main_list)
+
+print(f"Main List: {main_list}")
+
+input_indexes_string = input("Insert indexes> ")
+indexes_str_format = input_indexes_string.replace(" ", "")
+indexes_str_length = len(indexes_str_format)
+
+if indexes_str_length > main_list_length:
+    print("Error! Number of indexes <= length of list")
+else:    
+    multiplication_result = 1
+    
+    for i in range(indexes_str_length):
+        now_index = main_list[int(indexes_str_format[i])]
+        multiplication_result *= now_index
+    
+    print(f"Result of indexes multiplication: {multiplication_result}")
+
+    
+    
