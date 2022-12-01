2c57b8038bd7ca4f7a3ed2b45961753e86659dbc
oleg <poroshin.oa72@gmail.com>
2022-12-01 06:52:18 +0300

    **TASK_4** Complete

--- Seminar_3-DZ/task_4.py
+++ Seminar_3-DZ/task_4.py
@@ -3,3 +3,10 @@
 # - 45 -> 101101
 # - 3 -> 11
 # - 2 -> 10
+
+inp_int = int(input("Insert integer: "))
+result_binary = ""
+while inp_int != 0:
+    result_binary = str(inp_int % 2) + result_binary
+    inp_int //= 2
+print(result_binary)
