50f76b20b22532f57be087ed905e93a097b35204
oleg <poroshin.oa72@gmail.com>
2022-12-01 21:28:42 +0300

    **TASK_5** Complete

--- Seminar_3-DZ/task_5.py
+++ Seminar_3-DZ/task_5.py
@@ -1,3 +1,20 @@
 # Задайте число. Составьте список чисел Фибоначчи, в том числе для отрицательных индексов.
 # Пример:
 # - для k = 8 список будет выглядеть так: [-21 ,13, -8, 5, −3, 2, −1, 1, 0, 1, 1, 2, 3, 5, 8, 13, 21]
+
+
+# С отрицательными индексами не вышло =((
+fib_index = int(input("Insert fibonacci index> "))
+
+def fibonacci_plus(index):
+    fib_int1 = fib_int2 = 1
+
+    fib_plus_list = [fib_int1, fib_int2]
+    
+    for i in range(2, index):
+        fib_int1, fib_int2 = fib_int2, fib_int1 + fib_int2
+        fib_plus_list.append(fib_int2)
+
+    return fib_plus_list
+
+print(fibonacci_plus(fib_index))
