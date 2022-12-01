2ecca631b29da9856390b2f09e38b038bb84d4dd
oleg <poroshin.oa72@gmail.com>
2022-12-01 05:44:58 +0300

    **TASK_1** Complete

--- Seminar_3-DZ/task_1.py
+++ Seminar_3-DZ/task_1.py
@@ -1,3 +1,13 @@
 # Задайте список из нескольких чисел. Напишите программу, которая найдёт сумму элементов списка, стоящих на нечётной идексах.
 # Пример:
 # [2, 3, 5, 9, 3] -> на нечётных идексах элементы 3 и 9, ответ: 12
+
+original_list = [2, 3, 5, 9, 3, 5, 8, 11, 22]
+original_list_length = len(original_list)
+
+odd_elem_sum = 0
+
+for i in range(1, original_list_length, 2):
+    odd_elem_sum+=original_list[i]
+
+print(f"Odd sum: {odd_elem_sum}")
