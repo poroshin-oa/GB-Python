933f3bab125bbd80b4c425b4b4814f2bd3212273
oleg <poroshin.oa72@gmail.com>
2022-12-01 20:28:18 +0300

    **TASK_2** Complete

--- Seminar_3-DZ/task_2.py
+++ Seminar_3-DZ/task_2.py
@@ -2,3 +2,29 @@
 # Пример:
 # - [2, 3, 4, 5, 6] => [12, 15, 16];
 # - [2, 3, 5, 6] => [12, 15]
+
+def set_list_length(input_list):
+    input_list_length = len(input_list)
+
+    if input_list_length % 2 != 0:
+        input_list_length = input_list_length // 2 + 1
+    else:
+        input_list_length = input_list_length // 2
+
+    return input_list_length
+
+
+def multiply_list(inp_list, list_length):
+    result_list = []
+
+    for i in range(list_length):
+        result_list.append(inp_list[i] * inp_list[len(inp_list) - i - 1])
+
+    return result_list
+
+
+my_list = [2, 3, 4, 5, 6]
+my_list_len = set_list_length(my_list)
+mult_list = multiply_list(my_list, my_list_len)
+
+print(mult_list)
