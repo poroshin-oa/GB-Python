81e19808eb7f5f206da98cc00c5b25734123308b
oleg <poroshin.oa72@gmail.com>
2022-12-08 06:15:52 +0300

    **TASK_4** Complete

--- Seminar_4-DZ/task_4.py
+++ Seminar_4-DZ/task_4.py
@@ -4,3 +4,45 @@
 
 # - k=2 => 2*x² + 4*x + 5 = 0 или x² + 5 = 0 или 10*x² = 0
 # - k=4 => 8*(x**4) + 9*(x**3) + 1*(x**2) + 5*x + 4 = 0 или 8*(x**4) + 5*x + 4 = 0 и т.д.
+
+import random
+
+
+def write_file(st):
+    with open('result.txt', 'w') as data:
+        data.write(st)
+
+
+def rnd():
+    return random.randint(0,101)
+
+
+def create_mn(k):
+    lst = [rnd() for i in range(k+1)]
+    return lst
+    
+
+def create_str(sp):
+    lst= sp[::-1]
+    wr = ''
+    if len(lst) < 1:
+        wr = 'x = 0'
+    else:
+        for i in range(len(lst)):
+            if i != len(lst) - 1 and lst[i] != 0 and i != len(lst) - 2:
+                wr += f'{lst[i]}x^{len(lst)-i-1}'
+                if lst[i+1] != 0:
+                    wr += ' + '
+            elif i == len(lst) - 2 and lst[i] != 0:
+                wr += f'{lst[i]}x'
+                if lst[i+1] != 0:
+                    wr += ' + '
+            elif i == len(lst) - 1 and lst[i] != 0:
+                wr += f'{lst[i]} = 0'
+            elif i == len(lst) - 1 and lst[i] == 0:
+                wr += ' = 0'
+    return wr
+
+k = int(input("Введите натуральную степень k = "))
+koef = create_mn(k)
+write_file(create_str(koef))
