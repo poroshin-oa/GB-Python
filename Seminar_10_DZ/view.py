0208fc310006c2683f6ce098a4e078946a4f76d3
oleg <poroshin.oa72@gmail.com>
2022-12-29 06:05:35 +0300

    Промежуточный коммит

--- Seminar_10_DZ/view.py
+++ Seminar_10_DZ/view.py
@@ -1,4 +1,9 @@
 import model as core
+from prettytable import PrettyTable
 
 def show_contacts_list(non_format_contact_list):
-    print(non_format_contact_list)
+    contacts_table = PrettyTable()
+    contacts_table.field_names = ["ID", "ИМЯ", "ТЕЛЕФОН"]
+
+    contacts_table.add_row(["1", "Oleg", "8(900)546-17-05"])
+    print(contacts_table)
